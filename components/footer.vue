4b10aea45a56e63668dc1e6f5a4e62427280c37f
Artur Teixeira <arturjnt@gmail.com>
2021-07-19 16:25:32 +0100

    Update footer and delete unused packages

--- components/footer.vue
+++ components/footer.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="grid grid-cols-2 p-4 border-t-2 border-blue-900">
     <div>
-      Artur Teixeira - info@artur-teixeira.tech<br />
+      Artur Teixeira<br />
+      info@artur-teixeira.tech<br />
       <span
         class="underline text-blue-900 font-bold cursor-pointer"
         @click="scrollToTop"
@@ -10,13 +11,18 @@
       >
     </div>
     <div class="text-right">
-      read my articles on:
+      i post regularly on:
       <a href="https://www.linkedin.com/in/arturjnt/" target="_blank">
         LinkedIn
       </a>
       <br />
-      follow me on:
+      also, follow me on:
       <a href="https://twitter.com/arturjnt" target="_blank"> Twitter </a>
+      <br />
+      and check my open source work on:
+      <a href="https://github.com/arturjnt/" target="_blank">
+        GitHub
+      </a>
     </div>
   </div>
 </template>
